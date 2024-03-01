657095d9a49c606b4ae36c39d60a7573a9bb3561
Tonyliu2004 <ltsplice@gmail.com>
2024-03-01 16:59:25 -0500

    Image works

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,38 +1,37 @@
 import React from "react";
 import { useState, useEffect, forwardRef } from 'react';
-//import { createClient } from "pexels";
+import { createClient } from 'pexels';
 import './Card.css';
 
 const Card = ({ input, index }) => {
-    //console.log("cardinput: ",input);
     const [img, setImg] = useState("");
-    // const client = createClient(
-    //     import.meta.env.VITE_IMAGE_API_KEY
-    // );
+    const client = createClient(
+        import.meta.env.VITE_IMAGE_API_KEY
+    );
 
-    // useEffect(() => {
-    //     if (input === "") return;
+    useEffect(() => {
+        if (input === "") return;
 
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await fetch(`https://api.pexels.com/v1/search?per_page=1&query=${input}`, {
-    //                 headers: {
-    //                     Authorization: import.meta.env.VITE_IMAGE_API_KEY,
-    //                 }
-    //             });
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://api.pexels.com/v1/search?per_page=1&query=${input}`, {
+                    headers: {
+                        Authorization: import.meta.env.VITE_IMAGE_API_KEY,
+                    }
+                });
 
-    //             if (!response.ok) {
-    //                 alert('Network response was not ok');
-    //             }
-    //             const data = await response.json();
-    //             setImg(data.photos[0].src.original);
+                if (!response.ok) {
+                    alert('Network response was not ok');
+                }
+                const data = await response.json();
+                setImg(data.photos[0].src.original);
 
-    //         } catch (error) {
-    //             alert('Error fetching data:', error);
-    //         }
-    //     };
-    //     if (input != "") fetchData();
-    // }, [input]);
+            } catch (error) {
+                alert('Error fetching data:', error);
+            }
+        };
+        if (input != "") fetchData();
+    }, [input]);
     //console.log(img);
 
     return (
@@ -40,13 +39,9 @@ const Card = ({ input, index }) => {
             {img != "" &&
                 <div className="card">
                     <p id="card-text">{input}</p>
-                    {/* <img id="card-image" src={img} alt={input} /> */}
+                    <img id="card-image" src={img} alt={input} /> 
                 </div>
             }
-             {/* under this line - testing card with no image*/}
-            <div className="card">
-                <p id="card-text">{input}</p>
-            </div>
         </>
     )
 }

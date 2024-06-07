837b2900052866d6784038b7ea0d445fbf768597
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2024-06-07 17:22:54 -0400

    centered text for home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./Home.css";
+import "../styles/Home.css";
 
 function Home() {
     const navigate = useNavigate();
@@ -18,7 +18,7 @@ function Home() {
                 <div className="home__heading-text">
                     <h1>Travel like a LOCO</h1>
                     <p>What would you like to do today? The possibilities are endless.</p>
-                    <button onClick={() => navigate("/travel")}> Travel Generator </button>
+                    <button onClick={() => navigate("/travel")}>Travel Generator</button>
                 </div>
             </div>
         </div>
